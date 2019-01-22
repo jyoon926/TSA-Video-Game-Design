8e9b4adff02acf9dc58c4d7e93528faa2415c80b
Jacob Yoon <jacobyoon926@gmail.com>
2019-01-22 18:54:44 -0500

    Initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1548201123,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tm_tilemap1.png",
+		"images/player-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/playerlight-sheet0.png",
+		"images/wall-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
